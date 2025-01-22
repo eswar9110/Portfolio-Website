b714f3d645fcf92372670de635d46bab2babb628
eswar9110 <eswarnarayanak@gmail.com>
2025-01-22 14:42:12 +0530

    Add files via upload

---
+++ react-portfolio/src/data.js
@@ -0,0 +1,57 @@
+export const config = {
+    portfolio: {
+      portfolioLogo: 'Portfolio',
+      projectCard: {
+        cards: [
+          {
+            heading: 'Personalised ChatBot',
+            description:
+              'The Personalised ChatBot is a Streamlit-based web app that uses the Retrieval-Augmented Generation (RAG) framework and Googles Gemini-PRO to provide accurate, contextually relevant answers from uploaded PDFs. It allows users to interactively upload documents and receive detailed responses to their queries',
+            animationDelay: '300',
+            buttonLink: 'https://code-pal.netlify.app/chat',
+            buttonText: 'CHECK OUT',
+          },
+          {
+            heading: 'Code Pal Website',
+            description:
+              'Code Pal is an innovative learning platform that offers all its courses completely free, focusing on topics like coding, web development, cybersecurity, and artificial intelligence. Designed to break down financial and accessibility barriers, it empowers learners worldwide to develop valuable skills and advance their careers. Driven by a passion for democratizing education, Code Pal fosters a global community of enthusiastic learners ready to thrive in the digital age.',
+            animationDelay: '500',
+            buttonLink: 'https://code-pal.netlify.app/ ',
+            buttonText: 'CHECK OUT',
+          },
+          {
+            heading: 'Beer - Store',
+            description: 'I created a visually appealing and user-friendly Beer Store website using HTML and CSS. The website allows users to browse and explore various beer products through a clean and intuitive design. By utilizing responsive design principles, I ensured the website provides a seamless experience across different devices. This project helped me strengthen my skills in front-end development, focusing on layout design, styling, and creating a user-friendly interface.',
+            animationDelay: '700',
+            buttonLink: 'https://github.com/eswar9110/beer.git',
+            buttonText: 'CHECK OUT',
+          }
+        ],
+      },
+      internships: {
+        internship: [
+          {
+            companyName: 'TSNX',
+            role: 'Summer Internship',
+            introText: 'During my internship as a Full Stack Developer at TSNX, I gained comprehensive experience in designing, developing, and deploying scalable web applications. I contributed to both front-end and back-end development using technologies such as React.js, Node.js, and Express.js, while also integrating RESTful APIs to ensure seamless functionality. Additionally, I implemented DevOps practices, including setting up CI/CD pipelines and automated testing, to enhance deployment efficiency and maintain high code quality. My work also involved creating responsive, user-friendly interfaces with HTML, CSS, and JavaScript, along with gaining hands-on experience in cloud platforms like AWS and containerization tools such as Docker.',
+            duration: 'August - December 2024'
+          },
+          // {
+          //   companyName: 'ACIC Idea Labs',
+          //   role: 'Industrial Internship',
+          //   introText: 'At ACIC Idea Labs, from September to December 2023, I contributed to the development of "MindMate - Ultimate Summarizer," a machine learning web application designed to summarize various types of content, including PDFs, videos, websites, and plain text. I was also involved in business discussions regarding marketing, sales, and finance to ensure the product’s commercial viability.',
+          //   duration: 'September - December 2023'
+          // },
+          // {
+          //   companyName: 'VaultofCode',
+          //   role: 'Remote Internship',
+          //   introText: 'During my remote internship with VaultofCode from October to November 2023, I designed and developed an interactive web page using React. This role involved showcasing technical proficiency and user-centric design while managing the project independently from inception to deployment.',
+          //   duration: 'October - November 2023'
+          // },
+        ],
+      },
+    },
+  };
+  
+  export default config;
+  
