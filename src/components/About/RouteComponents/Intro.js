ccb535159941013dc10ab6073168f85c19671681
eswar9110 <eswarnarayanak@gmail.com>
2025-01-22 14:37:10 +0530

    Add files via upload

---
+++ src/components/About/RouteComponents/Intro.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import '../../../styles/About/Route/Intro.scss'
+
+const Intro = () => {
+    return (
+        <>
+            <div className="about-intro">
+                <div className="about-intro__container">
+                    <div className="about-intro__header">
+                        <h2 className="about-intro__number">0<span>1</span></h2>
+                        <h1 data-aos='fade-down' data-aos-offset="0">H<span>ello</span>.</h1>
+                    </div>
+                    <div className="about-intro__description">
+                        <p data-aos='fade-right'>Welcome to the universe of Eswar Narayana, where labels are just suggestions.</p>
+                        <p data-aos='fade-right' data-aos-delay='200'>Am I a cybersecurity enthusiast? A college student? A lifelong learner? A tech explorer? The answer is yes to all of the above and more.</p>
+                        <p data-aos='fade-right' data-aos-delay='400'>As a first-year student at Nxtwave Institute of Advanced Technology, I’ve already begun immersing myself in the fascinating world of cybersecurity, building a strong foundation in protecting and securing the digital universe.</p>
+                        <p data-aos='fade-right' data-aos-delay='600'>But my story doesn’t stop at mastering security protocols. I’m on a journey to learn, innovate, and explore new frontiers in technology while pushing the boundaries of my potential every step of the way.</p>
+                        <p data-aos='fade-right' data-aos-delay='800'>Oh, and did I mention I’m also passionate about solving problems and creating solutions that make a real-world impact? Whether it’s understanding the complexities of cyber threats or designing innovative defenses, I’m driven by a desire to make the digital world a safer place.</p>
+                        <p data-aos='fade-right' data-aos-delay='1000'>So, if you’re ready to embark on a journey of innovation, creativity, and a touch of cybersecurity brilliance, you’ve found the right place. Let’s learn, grow, and create a more secure digital future together! Let me know if you need further eits!</p>   
+
+                        <div className="about-intro__emoji">:&nbsp;)</div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Intro
